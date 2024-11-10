f03c11dd78221efdf14228e5f445e9ebc51961a3
Othmane Mahfoud <mahfoud.othmane97@gmail.com>
2024-11-10 18:37:08 +0100

    complete twitter lookup agent

--- agents/tools.py
+++
@@ -1,8 +0,0 @@
-from langchain_community.tools.tavily_search import TavilySearchResults
-
-
-def get_profile_url_tavily(name: str):
-    """Searches for Linkedin or Twitter Profile Page."""
-    search = TavilySearchResults()
-    res = search.run(f"{name}")
-    return res
